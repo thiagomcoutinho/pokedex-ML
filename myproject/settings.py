d290b75bbcb8c5120495c132e828914fd43b5e96
thiagomcoutinho <thiagomaltac@gmail.com>
2017-01-15 19:36:07 -0200

    ROOT_PATH atualization in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -111,3 +111,5 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
